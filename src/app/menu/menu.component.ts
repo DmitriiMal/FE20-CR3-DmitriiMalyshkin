ba7f1c72c37ea1cf38c18604d933f81328879698
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-21 11:51:24 +0200

    add products.ts, Iproducts.ts and fill in the objects

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Iproducts } from '../Iproducts';
+import { products } from '../products';
 
 @Component({
   selector: 'menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  products: Iproducts[] = products;
+}
