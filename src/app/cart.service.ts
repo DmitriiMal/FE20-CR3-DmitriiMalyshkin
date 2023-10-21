1abc871b8b71e236f724fa23c11077fc3807ba7d
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-21 14:19:41 +0200

    add addToCart, getCart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Iproducts } from './Iproducts';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  cart: Iproducts[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  addToCart(obj: Iproducts) {
+    this.cart.push(obj);
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  calcTotal() {
+    let total: number = 0;
+  }
 }
